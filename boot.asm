f866a9ae80c442fd67c8f343790cfa4ded1960b4
SumitK06032005 <sumitkumar1year@gmail.com>
2025-11-01 17:06:25 +0000

    Include files and func.asm

--- boot.asm
+++ boot.asm
@@ -1,21 +1,8 @@
-mov al, 'H'                      ; Here I was learning how to  
-call my_print_function           ;  how to implement calling a func in asm
-call some_function
-
-my_print_function:
-    mov ah, 0x0e
-    int 0x10
-    ret
-
-
-some_function :                  ; Sometimes, I mean a lot of times
-    pusha                        ; Register values are altered cause of
-    mov bx, 10                   ; our programming in asm and thus we 
-    add bx, 20                   ; are pushing and poping their original values
-    mov ah, 0x0e                 ; into stack to save them from being lost :)
-    int 0x10
-    popa
-    ret
+%include "my_print_function.asm"   
+                                ; Here we I was learning how to include 
+mov al, 'H'                     ; files so that we won't have to rewrite 
+call my_print_function          ; repetitive codes.
+jmp $
 
 times 510-($-$$) db 0
 dw 0xaa55
